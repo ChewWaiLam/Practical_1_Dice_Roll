b05758e45644c5fe99d2f54368db8cb21ee83c0e
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2021-06-22 17:24:18 +0800

    Second Submit:
    Full the requirement:
    - The app layout should include one text view and two buttons.
    - The app's code should set two click handlers, one for each button.
    - The click handler that resets the text view should set the text property to 0.
    - Coding Challenge (Optional)
    
    Question 1: Which method on an Activity inflates the app's layout and makes its views available as objects?
    - I think is onCreate()
    
    Question 2: Which view attribute do you use to set the width of a view so that it adjusts to fit the content?
    - I think is android:layout_width="wrap_content"

--- app/src/main/java/com/roes/diceroll/MainActivity.kt
+++ app/src/main/java/com/roes/diceroll/MainActivity.kt
@@ -8,19 +8,47 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    var randomInt = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollbutton: Button = findViewById(R.id.btn_Roll)
+        val btnCountUp: Button = findViewById(R.id.buttonCountUp)
+        val btnReset: Button = findViewById(R.id.buttonReset)
 
-            rollbutton.setOnClickListener(){
+        rollbutton.setOnClickListener(){
             RandomInt()
         }
+
+        btnCountUp.setOnClickListener(){
+            Increment()
+        }
+
+        btnReset.setOnClickListener(){
+            randomInt = 0
+            ShowString()
+        }
+
     }
 
     private fun RandomInt() {
-        val randomInt = (1..6).random()
+        randomInt = (1..6).random()
+        ShowString()
+
+    }
+
+    private fun Increment() {
+        if (randomInt >= 6) {
+            randomInt = 6
+        } else {
+            randomInt += 1
+        }
+        ShowString()
+    }
+
+    private fun ShowString() {
         val txtResult: TextView = findViewById(R.id.txt_Result)
         txtResult.text = randomInt.toString()
     }
