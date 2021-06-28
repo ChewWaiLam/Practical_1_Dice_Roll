9a150533545ac2bad64ced7e49a2e4070b5d912c
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2021-06-29 00:56:49 +0800

    Second Submit:
    Full the requirement:
    - The app layout should include one text view and two buttons.
    - The app's code should set two click handlers, one for each button.
    - The click handler that resets the text view should set the text property to 0.
    - Coding Challenge (Optional)
    
    Question 1: Which method on an Activity inflates the app's layout and makes its views available as objects?
    - I think is setContentView()
    
    Question 2: Which view attribute do you use to set the width of a view so that it adjusts to fit the content?
    - I think is android:layout_width="wrap_content"

--- app/src/main/java/com/roes/diceroll/MainActivity.kt
+++ app/src/main/java/com/roes/diceroll/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
         val rollbutton: Button = findViewById(R.id.btn_Roll)
